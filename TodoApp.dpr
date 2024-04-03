a1391970174cbdbc7e4a947687540b93d6ffcfb6
Errol McFadden <errol.mcfadden@multinail.com>
2024-04-04 09:40:16 +1000

    Blank project

---
+++ TodoApp.dpr
@@ -0,0 +1,17 @@
+program TodoApp;
+
+uses
+  Vcl.Forms,
+  MainForm in 'MainForm.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 SlateGray');
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
